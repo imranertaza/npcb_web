faef3aac5f10142a03c6dbf00359caf7bed122fc
dnationsoftdm6 <dnationsoftdm6@gmail.com>
2025-11-30 21:13:25 +0600

    working on home about comitee and contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,95 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".custom-swiper-button-prev",
   },
 });
+
+var gamingSwiper = new Swiper(".myGamingSwiper", {
+  loop: true,
+  spaceBetween: 24,
+  navigation: {
+    nextEl: ".custom-swiper-button-next",
+    prevEl: ".custom-swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1, // mobile
+    },
+    576: {
+      slidesPerView: 2, // small tablets
+    },
+    768: {
+      slidesPerView: 3, // tablets
+    },
+    992: {
+      slidesPerView: 4, // desktops
+    }
+  }
+});
+
+var runningEventSwiper = new Swiper(".myRunningEventSwiper", {
+  loop: true,
+  spaceBetween: 24,
+  navigation: {
+    nextEl: ".custom-swiper-button-next",
+    prevEl: ".custom-swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    }
+  }
+});
+
+var upcomingEventSwiper = new Swiper(".myUpcomingEventSwiper", {
+  loop: true,
+  spaceBetween: 24,
+  navigation: {
+    nextEl: ".custom-swiper-button-next",
+    prevEl: ".custom-swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    }
+  }
+});
+
+var latestBlogSwiper = new Swiper(".myLatestBlogSwiper", {
+  loop: true,
+  spaceBetween: 24,
+  navigation: {
+    nextEl: ".custom-swiper-button-next",
+    prevEl: ".custom-swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 3,
+    }
+  }
+});
